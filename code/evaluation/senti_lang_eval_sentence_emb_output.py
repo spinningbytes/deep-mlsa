67536faa16f8669f030dae068bf1a476966b074b
Jan Deriu <jan.deriu@gmail.com>
2017-03-02 13:37:35 +0100

    finalized code

--- code/evaluation/senti_lang_eval_sentence_emb_output.py
+++ code/evaluation/senti_lang_eval_sentence_emb_output.py
@@ -1,3 +1,7 @@
+from keras.utils.np_utils import probas_to_classes
+from sklearn.metrics import f1_score
+import numpy as np
+
 def evaluate(model, test_iteraotr, experiment_name, ofname):
     inputs = test_iteraotr.input_data
     outputs = test_iteraotr.output_data
@@ -5,13 +9,31 @@ def evaluate(model, test_iteraotr, experiment_name, ofname):
 
     for i, o, n in zip(inputs, outputs, names):
         ofile = open(ofname, 'wt')
+        y_test_senti = probas_to_classes(o[0])
 
-        sentence_embeddings = model.predict(i)
+        model_output = model.predict(i)
+        sentence_embeddings = model_output[1]
+        y_pred = model_output[0]
+
+        y_pred_senti = y_pred
+
+        y_pred_senti_cls = probas_to_classes(y_pred_senti)
+
+        f1_score_senti = f1_score(y_test_senti, y_pred_senti_cls, average=None, pos_label=None, labels=[0, 1, 2])
+
+        output_line = '{}\n'.format(n)
+        output_line += 'Sentiment:\tF1 Score:{}\tF1 Score SEval:{}\tF1 Score Neg:{}\tF1 Score Neut:{}\tF1 Score Pos:{}\n'.format(
+            np.mean(f1_score_senti),
+            0.5 * (f1_score_senti[0] + f1_score_senti[2]),
+            f1_score_senti[0],
+            f1_score_senti[1],
+            f1_score_senti[2]
+        )
 
         for list in sentence_embeddings:
             slist = [str(x) for x in list.tolist()]
 
-            output_line = '{}\n'.format(' '.join(slist))
-            ofile.write(output_line)
+            file_output_line = '{}\n'.format(' '.join(slist))
+            ofile.write(file_output_line)
 
     return output_line
